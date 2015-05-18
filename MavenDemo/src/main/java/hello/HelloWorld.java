c8c9d0b3561e5d4c73a84156c2d8fb333f5cb56c
Thomas Patterson <tompatt92@gmail.com>
2015-05-18 08:42:53 -0400

    Added methods to Greeter and HelloWorld Classes for Demo

---
+++ MavenDemo/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+
+}
